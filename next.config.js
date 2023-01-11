e7da4d3365225332783eae23f648ca42489da9f3
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2023-01-12 01:11:42 +0530

    Dockerized the app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   webpackDevMiddleware: config => {
+//     config.watchOptions = {
+//       poll: 1000,
+//       aggregateTimeout: 300,
+//     }
+//     return config
+//   },
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  // webpackDevMiddleware: config => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300,
+  //   }
+
+  //   return config
+  // },
+  experimental: {
+    outputStandalone: true,
+  },
+}
